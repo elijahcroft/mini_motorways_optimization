7991797c71d5c423e411fcc4a128d8c07868144c
elijahcroft <elijahcroft49@gmail.com>
2024-12-20 23:48:26 -0800

    Finished some very basic stuff getting the ball rolling

---
+++ pathfinding.py
@@ -0,0 +1,11 @@
+import pyautogui
+pyautogui.screenshot("screenshot.png")
+
+
+
+
+
+#the goal is to locate the houses and the stores on the game and use an algo to find optimal path
+
+for i in pyautogui.locateAllOnScreen("beigehouse.png"):
+    pass
